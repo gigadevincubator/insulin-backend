881f601457a77cb97bb5d66dd4ea3010b6c4dea2
Marin Bilba <maryn777@gmail.com>
2021-02-20 22:50:38 +0100

    Added user Controller

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using insulin_backend.Database.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace insulin_backend.Controllers
+{
+    [ApiController]
+    //todo create common root path (patient) for all methods //  [Route("patient")]
+    
+    public class UsersController : ControllerBase
+    {
+        [HttpGet("users/{userId}/tutorials")]
+        //todo replace object return 
+        public async Task<ActionResult<IList<Tutorial>>> GetAllUsersTutorialsAsync([FromRoute] int userId)
+        {
+            
+            Console.WriteLine("Here" + userId);
+        
+      
+            return null;
+        }
+    }
+}
