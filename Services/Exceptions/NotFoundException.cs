f7ebdcb8f9c1f3b8fe882319e22a73b379974bf2
Marin Bilba <maryn777@gmail.com>
2021-02-23 00:34:49 +0100

    Added constructor for passing exception messages

--- Services/Exceptions/NotFoundException.cs
+++ Services/Exceptions/NotFoundException.cs
@@ -4,6 +4,11 @@ namespace insulin_backend.Services.Exceptions
 {
     public class NotFoundException : Exception
     {
+        public NotFoundException()
+        {
+        }
+
+        public NotFoundException(string message) : base(message) { }
         
     }
 }
