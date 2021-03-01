fcd6a994f67f53a2d69097f7ad5bb7accad6425b
Marin Bilba <marynbilba@gmail.com>
2021-03-01 13:36:16 +0100

    Added azure service with the functionality of uploading a file to the BLOB storage

---
+++ Services/AzureService/IAzureBlobService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace insulin_backend.Services.AzureService
+{
+    public interface IAzureBlobService
+    {
+        Task UploadAsync(IFormFileCollection files);
+
+    }
+}
