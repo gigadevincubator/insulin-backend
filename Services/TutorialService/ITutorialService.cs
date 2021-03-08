f29833a1eb32ba366ff833613c5fd8336ddf21e1
Marin Bilba <marynbilba@gmail.com>
2021-03-08 22:59:07 +0100

    Added find tutorialById method

---
+++ Services/TutorialService/ITutorialService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using insulin_backend.Database.Models;
+
+namespace insulin_backend.Services.TutorialService
+{
+    public interface ITutorialService
+    {
+        Tutorial FindTutorialById(int tutorialId);
+    }
+}
