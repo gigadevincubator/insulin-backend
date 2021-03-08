7731bae43f3dc72268ad63751d80e82de725ca8e
Marin Bilba <marynbilba@gmail.com>
2021-03-08 22:00:53 +0100

    Refactored namespaces

---
+++ Services/TutorialLanguageService/ITutorialLanguageSerive.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace insulin_backend.Services.TutorialLanguageService
+{
+    public interface ITutorialLanguageSerive
+    {
+        public Object GetTutorialLanguageByTitle(string title, int languageId);
+    }
+}
